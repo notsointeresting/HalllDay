85177983acec62be6ec588392e76a9e42c0cf7b2
notsointeresting <eli311.lucero@gmail.com>
2025-12-07 19:12:40 -0600

    fix: correct models/__init__.py to use factory pattern export

--- models/__init__.py
+++ models/__init__.py
@@ -1,4 +1,5 @@
 # Models package initialization
-from .user import User
+# User model uses factory pattern - import create_user_model, not User directly
+from .user import create_user_model
 
-__all__ = ['User']
+__all__ = ['create_user_model']
